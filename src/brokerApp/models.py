72fee3c4957194e25677e1e12297f05e219a00fd
WinterOdin <cczuryszkiewicz@gmail.com>
2020-11-24 23:51:51 +0100

    working on filter
    Sooooo... I wanted to do it with react but I've changed my mind  midway through so I'm sticking with django forms and some custom html&css to suit my needs

--- src/brokerApp/models.py
+++ src/brokerApp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Property(models.Model):
+    TYPE_OF_PLACE = [
+        ('Apartment', 'Apartment'),
+        ('Townhouse', 'Townhouse'),
+        ('Whole bulding', 'Whole bulding'),
+        ('Loft', 'Loft'),
+    ]
+    ROOMS = [
+        ('Studio', 'Studio'),
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4+'),
+    ] 
+
+    about       = models.TextField(max_length=355, null=True, blank=True)
+    place_type  = models.CharField(max_length=20, choices=TYPE_OF_PLACE)
+    city        = models.CharField(max_length=50, null=True, blank=True)
+    street      = models.CharField(max_length=70, null=True, blank=True)
+    size        = models.IntegerField(null=True, blank=True)
+    beds        = models.CharField(max_length=10, null=True, choices=ROOMS)
+    baths       = models.IntegerField(null=True, blank=True)
+    price       = models.CharField(max_length=10, null=True, blank=True)
+    
+    
+    
--- src/brokerApp/views.py
+++ src/brokerApp/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .filters import *
 
 
 
@@ -12,7 +13,14 @@ def home(request):
     return render(request,'home.html', context)
 
 def apartments(request):
+    property_data = Property.objects.all()
+    myFilter = PropertyFilter()
+    if request.method == 'GET':
+        filtera = PropertyFilter(request.GET, queryset=property_data)
+        property_data = filtera.qs
 
-
-    context={}
+    context={
+        'myFilter':myFilter,
+        'data':property_data
+    }
     return render(request,'broker.html', context)
